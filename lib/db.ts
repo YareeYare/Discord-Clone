b220b0f48f20d0f857cb96e45eb09a57a2db33eb
YareeYare <mehulshakya230@gmail.com>
2024-06-12 08:28:44 +0530

    connect to DB and add prisma schema for Profile, Server and Members

---
+++ lib/db.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@prisma/client'
+
+// All this due to 'Hot Reload'
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+const db = globalThis.prisma ?? prismaClientSingleton()
+
+export default db
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = db
