eef6960acc0630b00e169d45d7d96628cef5f2cf
YareeYare <mehulshakya230@gmail.com>
2024-06-13 13:27:25 +0530

    add server creation api

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,10 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
 
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "@/app/api/uploadthing/core";
+
 const font = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -25,6 +29,15 @@ export default function RootLayout({
 					font.className,
 					"bg-white dark:bg-[#313338]"
 				) }>
+			      	<NextSSRPlugin
+						/**
+						* The `extractRouterConfig` will extract **only** the route configs
+						* from the router to prevent additional information from being
+						* leaked to the client. The data passed to the client is the same
+						* as if you were to fetch `/api/uploadthing` directly.
+						*/
+					routerConfig={extractRouterConfig(ourFileRouter)}
+					/>
 					<ThemeProvider
 						attribute="class"
 						defaultTheme="dark"
