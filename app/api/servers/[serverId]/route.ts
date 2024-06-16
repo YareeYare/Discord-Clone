5a6aac7a8157ba8ef916425f86c5704a67c6f75a
YareeYare <mehulshakya230@gmail.com>
2024-06-16 19:50:58 +0530

    add edit server modal

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -1,44 +1,38 @@
-import { NextResponse } from "next/server"
-import { v4 as uuidv4 } from "uuid"
-
 import { currentProfile } from "@/lib/current-profile"
 import db from "@/lib/db"
+import { NextResponse } from "next/server"
 
-interface PatchParams {
-	params: {
-		serverId: string
-	}
-}
-
-export async function PATCH(
+export async function  PATCH(
 	req: Request,
-	{ params }: PatchParams
+	{ params }: { params: { serverId: string } }
 ) {
 	try {
 		const profile = await currentProfile()
+
+		const { name, imageUrl } = await req.json()
 
 		if( !profile ) {
 			return new NextResponse("Unauthorized", { status: 401 })
 		}
 
-		if( !params.serverId ) {
-			return new NextResponse("Server ID Missing", { status: 400 })
-		}
-
 		const server = await db.server.update({
 			where: {
 				id: params.serverId,
-				profileId: profile.id // means only Admin can do this
+				profileId: profile.id // only Admin can modify server
 			},
 			data: {
-				inviteCode: uuidv4(),
+				name,
+				imageUrl,
 			}
 		})
 
 		return NextResponse.json( server )
 
 	} catch( error ) {
-		console.log("[SERVER_ID]", error)
+		console.log("[SERVER_ID_PATCH]", error)
 		return new NextResponse("Internal Error", { status: 500 })
 	}
+
+
+
 }
